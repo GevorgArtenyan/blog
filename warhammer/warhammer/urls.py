765835195c1fc6f6955012e22fbfdf078f01c64b
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2019-12-20 17:36:21 +0400

    Users registration, logion, logout and profile created.

--- warhammer/warhammer/urls.py
+++ warhammer/warhammer/urls.py
@@ -14,13 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from user import views as user_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('warhammerwiki.urls')),
+    path('register/', user_view.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('profile/', user_view.profile, name='profile')
 ]
 
 if settings.DEBUG:
