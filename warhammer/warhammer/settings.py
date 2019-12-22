9b71b97161bf73364375d6c7a13674142a3605a4
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2019-12-22 18:27:03 +0400

    Users can update their profile now.

--- warhammer/warhammer/settings.py
+++ warhammer/warhammer/settings.py
@@ -107,9 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',  # enables simple command line authentication
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
--- warhammer/warhammerwiki/views.py
+++ warhammer/warhammerwiki/views.py
@@ -11,13 +11,12 @@ import json
 from django.contrib.auth.models import User
 from .serializers import UnitSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
-
+from rest_framework.permissions import IsAuthenticated
 class UnitAPIListView(generics.ListAPIView):
     lookup_field = 'pk'
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
 
 
 
